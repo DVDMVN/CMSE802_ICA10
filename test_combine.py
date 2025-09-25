bc3f680bd978a225f3bf4a612cb21e71b0c4f057
DillonJHaller <hallerdi@msu.edu>
2025-09-25 09:47:11 -0400

    Added second unit test to test_combine

--- test_combine.py
+++ test_combine.py
@@ -7,3 +7,7 @@ class TestCombineHandles(unittest.TestCase):
     #First test case: All valid emails
     def test_all_valid_emails(self):
         self.assertEqual(combine_handles(["a@msu.edu", "b@msu.edu", "c@msu.edu"]), "a,b,c")
+
+    #Second test case: Non-email strings
+    def test_non_email_strings(self):
+        self.assertEqual(combine_handles(["test@msu.edu", "invalid_email", "test2@msu.edu"]), "test,test2")
